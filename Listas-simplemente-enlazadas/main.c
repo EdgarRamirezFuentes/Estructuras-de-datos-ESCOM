13c364bce0420a2c14904e7d448927179c484a89
Edgar Ramirez <edgar-alejandro-fuentes@hotmail.com>
2019-09-22 14:38:08 -0500

    Versión 1.1 de Listas simplemente enlazadas.

--- Listas-simplemente-enlazadas/main.c
+++ Listas-simplemente-enlazadas/main.c
@@ -1,178 +1,164 @@
+/**
+ \file main.c
+ \author Edgar Alejandro Ramírez Fuentes
+ \version 1.1
+ \last update date 22 / 09 / 2019
+ \copyright GNU Public License v3.
+ Implementación de las funciones básicas
+ de una lista simplemente ligada:
+ + AgregarNuevoNodo
+ + AgregarNodoAlInicio
+ + AgregarNodoAlFinal
+ + MostrarLista
+ + EliminarNodo
+ + EliminarNodoAlFinal
+ + EliminarNodoAlInicio
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 struct Nodo {
 	int dato;
 	struct Nodo *siguiente;
 };
 
-// Muestra los elementos de la lista enviada como parámetro.
-void MostrarLista(struct Nodo *nodo, int totalDatos);
+void
+MostrarLista(struct Nodo *nodo);
 
-// Agrega un nuevo nodo al después del último nodo con dato asignado.
-struct Nodo* AgregarNuevoNodo(struct Nodo *nodo, int dato, int opcion, bool *hayMemoria, int *totalDatos);
+// Agrega un nuevo nodo en la lista.
+// El usuario decide si agregarla al inicio o al final de la lista. 
+struct Nodo* 
+AgregarNuevoNodo(struct Nodo *nodo, int dato, int opcion);
 
-int main(){
-	struct Nodo *nodo = (struct Nodo*) calloc(1 , sizeof(struct Nodo));
-       	nodo -> siguiente = NULL;
-	int opcion;
-	int dato;
-	bool hayMemoria = true;
-	int totalDatos = 0;
-	bool repetir = true;
-	do{
-		system("clear");
-		printf("Listas enlazadas.");
-		printf("\n1. Agregar al inicio de la lista.\n2. Agregar al final de la lista.\n3. Agregar en una posición específica\n");
-		printf("4. Mostrar la lista.");
-		printf("\n¿Qué operación deseas hacer? ");
-		scanf("%d", &opcion);
-		switch(opcion){
-			case 1:
-			       	system("clear");	
-				if(hayMemoria){
-					printf("Ingresa el dato a agregar en la lista: ");
-					scanf("%d",&dato);
-					nodo = AgregarNuevoNodo(nodo, dato, opcion, &hayMemoria, &totalDatos);
-				}else{
-					printf("No hay memoria para agregar otro dato");
-				}
-			break;
-			case 2:
-				system("clear");
-				if(hayMemoria){
-					printf("Ingresa el dato que se va a agregar al final de la lista: ");
-					scanf("%d", &dato);
-					AgregarNuevoNodo(nodo, dato, opcion, &hayMemoria, &totalDatos);
+struct Nodo*
+AgregarNodoAlFinal(struct Nodo *nodo, int dato);
 
-				}else{
-					printf("No hay memoria para agregar otro dato");
-				}
-			break;
-			case 3:
-				system("clear");
-				if(hayMemoria){
-					printf("Ingresa el dato que se va a agregar en una posición específica: ");
-					scanf("%d", &dato);
-					AgregarNuevoNodo(nodo, dato, opcion, &hayMemoria, &totalDatos);
-				}else{
-					printf("No hay memoria para agregar otro dato.");
-				}
-			break;
-			case 4:
-				system("clear");
-				MostrarLista(nodo, totalDatos);
-			break;
-			default:
-				system("clear");
-				printf("Opción no válida.");
-			break;
-		}
-		system("clear");
-		printf("\n1. Sí\n2. No");
-		printf("\n¿Deseas hacer otra operación? ");
-		scanf("%d", &opcion);
-		if(opcion != 1){
-			repetir = false;
-		}
-	}while(repetir != false);
+struct Nodo* 
+AgregarNodoAlInicio(struct Nodo *nodo, int dato);
+
+// Elimina un nodo en la lista.
+// El usuario decide si eliminarlo al inicio o al final de la lista. 
+struct Nodo*
+EliminarNodo(struct Nodo *nodo, int opcion);
+
+struct Nodo*
+EliminarNodoAlInicio(struct Nodo *nodo);
+
+struct Nodo*
+EliminarNodoAlFinal(struct Nodo *nodo);
+
+int 
+main(void){
+	struct Nodo *nodo = NULL;
+	nodo = AgregarNuevoNodo(nodo,0,1);
+	MostrarLista(nodo);
+	nodo = AgregarNuevoNodo(nodo,1,2);
+	MostrarLista(nodo);
+	nodo = AgregarNuevoNodo(nodo,-1,1);
+	MostrarLista(nodo);
+	nodo = EliminarNodo(nodo, 1);
+	MostrarLista(nodo);
+	nodo = EliminarNodo(nodo, 2);
+	MostrarLista(nodo);
 	return 0;
 }
 
-struct Nodo* AgregarNuevoNodo(struct Nodo *nodo, int dato, int opcion, bool *hayMemoria, int *totalDatos){
-	int posicion = 0;
+struct Nodo* 
+AgregarNuevoNodo(struct Nodo *nodo, int dato, int opcion){ // Complejidad O(1)
 	switch(opcion){
 		case 1:
-			if(*totalDatos == 0){
-				printf("1");
-				nodo -> dato = dato;
-				*totalDatos += 1;
-				printf("Se agregó correctamente el primer elemento de la lista");
-				return nodo;
-			}else{
-				printf("2");
-				printf("Entré al segundo");
-				struct Nodo *nuevoNodo;
-				nuevoNodo = (struct Nodo*) calloc(1 , sizeof(struct Nodo));
-				if(nuevoNodo != NULL){
-					nuevoNodo -> siguiente = nodo;
-					nuevoNodo -> dato = dato;
-					*hayMemoria = true;
-					*totalDatos += 1;
-					printf("Se agregó correctamente al inicio de la lista.");
-					return nuevoNodo;				
-				}else{
-					printf("No se pudo agregar el nuevo dato, falta memoria");
-					*hayMemoria = false;
-					return nodo;
-				}
-			}
-
+			return AgregarNodoAlInicio(nodo, dato);
 		break;
 		case 2:
-			if(totalDatos == 0){
-				nodo -> dato = dato;
-				*totalDatos += 1;
-				printf("Se agregó correctamente el primer elemento de la lista");
-				return nodo;
-			}else{
-				struct Nodo *nuevoNodo;
-				nuevoNodo = (struct Nodo*) calloc(1, sizeof(struct Nodo));
-				if(nuevoNodo != NULL){
-					while(nodo -> siguiente != NULL){
-						nodo = nodo -> siguiente;
-					}
-					nodo -> siguiente = nuevoNodo;
-					nuevoNodo -> dato = dato;
-					nuevoNodo -> siguiente = NULL;
-					printf("Dato agregado correctamente al final de la lista");
-					*hayMemoria = true;
-					*totalDatos += 1;
-					return nodo;
-				}else{
-					printf("No se pudo agregar el nuevo dato, falta memoria.");
-					*hayMemoria = false;
-					return nodo;
-				}	
-			}
-			
+			return AgregarNodoAlFinal(nodo, dato);
 		break;
-		case 3:
-			if(*totalDatos == 0){
-				printf("Se agregó correctamente el primer dato de lista.");
-			}else{
-				MostrarLista(nodo, *totalDatos);
-				printf("Ingresa la posición en la que se insertará el dato: ");
-				scanf("%d", &posicion);
-				if(posicion == 0){
-					AgregarNuevoNodo(nodo, dato, opcion, hayMemoria, totalDatos);
-				}else{
-					for(int i = 0; i < posicion; i++){
-						nodo = nodo -> siguente;
-					}
-					struct Nodo *nuevoNodo =  (struct Nodo*) calloc(1,sizeof(struct Nodo));
-					if(nuevoNodo != NULL){
-							
-					}else{
-
-					}
-				}
-			}
-		break;
+	}	
+	return nodo;
+}
+struct Nodo*
+AgregarNodoAlInicio(struct Nodo *nodo, int dato){ // Complejidad O(1)
+	struct Nodo *nuevoNodo = (struct Nodo*)malloc(sizeof(struct Nodo));// O(1);
+	if(nuevoNodo != NULL){
+		nuevoNodo -> dato = dato; // O(1)
+		if(nodo != NULL){
+			nuevoNodo -> siguiente = nodo; // O(1);
+		}
+		return nuevoNodo; // O(1)
+	}
+	return nodo; // O(1)
+}
+struct Nodo*
+AgregarNodoAlFinal(struct Nodo* nodo, int dato){ // Complejidad O(n)
+	struct Nodo* tmp = NULL; // O(1)
+	struct Nodo* nuevoNodo = (struct Nodo*)malloc(sizeof(struct Nodo)); // O(1)
+	if(nuevoNodo != NULL){
+		nuevoNodo -> dato = dato; // O(1)
+		if(nodo == NULL){
+			return nuevoNodo; // O(1)
+		}
+		tmp = nodo; // O(1)
+		while(tmp -> siguiente != NULL){ // O(n)
+			tmp = tmp -> siguiente; // O(1)
+		}
+		tmp -> siguiente = nuevoNodo; //O(1)
+		return nodo; // O(1)
+	}
+	return nodo; // O(1)
+}
+void 
+MostrarLista(struct Nodo* nodo){ // Complejidad O(n)
+	if(nodo != NULL){
+		while(nodo != NULL){ // O(n)
+			printf("| %d |", nodo -> dato); // O(1)
+			nodo = nodo -> siguiente;
+		}
+		printf("\n"); // O(1)
+	}else{
+		printf("No hay datos en la lista"); // O(1)
 	}
 }
 
-void MostrarLista(struct Nodo *nodo, int totalDatos){
-	if(totalDatos == 0){
-		printf("No hay datos agregados en la lista.");
-	}else{
-		if(nodo -> siguiente != NULL){
-			while(nodo != NULL){
-				printf("| %d |", nodo -> dato);
-				nodo = nodo -> siguiente;
-			}
-		}else{
-			printf("| %d |", nodo -> dato);
-		}
+struct Nodo*
+EliminarNodo(struct Nodo *nodo, int opcion){ // Complejidad O(1)
+	switch(opcion){
+		case 1:
+			return EliminarNodoAlInicio(nodo);
+		break;
+		case 2:
+			return EliminarNodoAlFinal(nodo);
+		break;
 	}
+	return nodo;
+}
+
+struct Nodo* 
+EliminarNodoAlInicio(struct Nodo *nodo){ // Complejidad O(1)
+	struct Nodo *tmp = NULL; // O(1)
+	if(nodo != NULL){
+		tmp = nodo; //O(1) 
+		nodo = nodo -> siguiente; //O(1)
+		free(tmp); // O(1)
+		return nodo; // O(1)
+	}
+	return nodo; // O(1)
+}
+
+struct Nodo*
+EliminarNodoAlFinal(struct Nodo *nodo){ // Complejidad O(n)
+	struct Nodo *tmp = NULL; // O(1)
+	if(nodo != NULL){
+		if(nodo -> siguiente == NULL){ // 0(1)
+			free(nodo); // O(1)
+			return NULL; //O(1)
+		}
+		tmp = nodo; // O(1)
+		while(tmp -> siguiente -> siguiente != NULL){ // O(n)
+			tmp = tmp -> siguiente; // O(1)
+		}
+		free(tmp -> siguiente); // O(1)
+		tmp -> siguiente = NULL;
+		tmp -> siguiente = NULL;
+		return nodo; // O(1)
+	}
+	return nodo;
 }
