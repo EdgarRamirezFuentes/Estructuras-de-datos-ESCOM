2d8e63c71fcb7e8ef6b3b7b92d65fa021147273b
Edgar Ramirez <edgar-alejandro-fuentes@hotmail.com>
2019-09-24 03:39:19 -0500

    Version 1.0 de Listas circulares simplemente enlazadas.

---
+++ Listas-Circulares-Simplemente-Enlazadas/main.c
@@ -0,0 +1,104 @@
+ 
+/**
+ \file main.c
+ \author Edgar Alejandro Ramírez Fuentes
+ \version 1.0
+ \last update date 24 / 09 / 2019
+ \copyright GNU Public License v3.
+ Implementación de las funciones básicas
+ de una lista circular simplemente enlazada:
+ + AgregarDato
+ + AgregarDatoAlInicio
+ + AgregarDatoAlFinal
+ + EliminarDato
+ + EliminarDatoAlInicio
+ + EliminarDatoAlFinal
+ + MostrarLista
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Nodo {
+	int dato;
+	struct Nodo *siguiente;
+};
+
+// Agrega un dato a la lista al inicio o al final de ella (El usuario elige la posición).
+struct Nodo*
+AgregarDato(struct Nodo *nodo, int dato, int opcion);
+
+struct Nodo*
+AgregarDatoAlInicio(struct Nodo *nodo, int dato);
+
+void 
+MostrarLista(struct Nodo *nodo);
+/** TODO LIST
+struct Nodo*
+AgregarDatoAlFinal(struct Nodo *nodo, int opcion);
+
+struct Nodo*
+EliminarDato(struct Nodo *nodo, int dato, int opcion);
+
+struct Nodo*
+EliminarDatoAlInicio(struct Nodo *nodo, int dato);
+
+struct Nodo*
+EliminarDatoAlFinal(struct Nodo *nodo, int dato);
+*/
+int 
+main(void){
+	struct Nodo *nodo = NULL;
+	MostrarLista(nodo);
+	nodo = AgregarDato(nodo, 3, 1);
+	MostrarLista(nodo);
+	nodo = AgregarDato(nodo, 2,1);
+	MostrarLista(nodo);
+	return 0;
+}
+
+struct Nodo*
+AgregarDato(struct Nodo *nodo, int dato, int opcion){
+	switch(opcion){
+		case 1:
+			return AgregarDatoAlInicio(nodo, dato);
+		break;
+		case 2: 
+			//TODO: return AgregarNodoAlFinal(nodo, dato);
+		break;
+	}
+	return nodo;
+}
+
+struct Nodo*
+AgregarDatoAlInicio(struct Nodo *nodo, int dato){
+	struct Nodo *nuevoNodo = (struct Nodo*)malloc(sizeof(struct Nodo));
+	if(nuevoNodo != NULL){
+		nuevoNodo -> dato = dato;
+		if(nodo == NULL){
+			nuevoNodo -> siguiente = nuevoNodo;
+			return nuevoNodo;
+		}
+		struct Nodo *tmp = nodo;
+		nuevoNodo -> siguiente = nodo;
+		while(tmp -> siguiente != nodo){
+			tmp = tmp -> siguiente;
+		}
+		tmp -> siguiente = nuevoNodo;
+		return nuevoNodo;
+	}
+	return nodo;
+}	
+
+void 
+MostrarLista(struct Nodo *nodo){
+	if(nodo != NULL){
+		struct Nodo *tmp = nodo;
+		do{
+			printf("| %d |", tmp -> dato);
+			tmp = tmp -> siguiente;
+		}while(tmp != nodo);
+	}else{
+		printf("La lista está vacía");
+	}
+	printf("\n");
+}
